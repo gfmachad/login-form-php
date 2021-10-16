a4ebfe94562c858dfd6781d3911c3d2ca93f8bc9
Guilherme Fritzen Machado <75760100+gfmachad@users.noreply.github.com>
2021-10-15 22:23:34 -0300

    First commit

---
+++ welcome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+  <title>Welcome</title>
+</head>
+<?php
+
+include('checkLogin.php');
+
+echo "<div class='container'>
+<p>Welcome, take the opportunity to learn a little about Uruguay</p>
+<a href='https://gfmachad.github.io/Parallax-project/'>Parallax Uruguai!</a>
+<br>
+<a href='logout.php'>LOGOUT</a>
+
+</div>"
+
+?>
+
+<body>
+
+</body>
+
+</html>
